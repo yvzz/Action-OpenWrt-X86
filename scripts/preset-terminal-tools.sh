627126fcfdc4d3e15c8fcb58fb7c73a45327186e
Macmini <>
2026-06-18 21:55:40 +0800

    no message

--- scripts/preset-terminal-tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# preset-terminal-tools.sh — 终端工具预设
-# TTYD 免登录
-if [ -f feeds/packages/utils/ttyd/files/ttyd.config ]; then
-  sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
-fi
